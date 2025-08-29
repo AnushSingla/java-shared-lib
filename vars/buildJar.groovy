358179b48f6585e37eba71d987f7e2d0911b53a1
AnushSingla <singlaanush18@gmail.com>
2025-08-29 12:53:35 +0530

    fix

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,14 +1,28 @@
 #!/usr/bin/env groovy
-def call(){
+def call() {
     echo "Building Application"
-    sh "mvn build-helper:parse-version"
-    def majorVersion = sh(script :  "mvn help:evaluate -Dexpression=parsedVersion.majorVersion  -q -DforceStdout",returnStdout:true).trim()
-    def minorVersion = sh(script :  "mvn help:evaluate -Dexpression=parsedVersion.minorVersion  -q -DforceStdout",returnStdout:true).trim()
-    def patch = sh(script: "mvn help:evaluate -Dexpression=parsedVersion.nextIncrementalVersion -q -DforceStdout", returnStdout: true).trim()
-    def fullVersion = "${majorVersion}.${minorVersion}.${patch}"
+
     
+    sh "mvn build-helper:parse-version"
+
+    
+    def majorVersion = sh(script: "mvn help:evaluate -Dexpression=parsedVersion.majorVersion -q -DforceStdout", returnStdout: true).trim()
+    def minorVersion = sh(script: "mvn help:evaluate -Dexpression=parsedVersion.minorVersion -q -DforceStdout", returnStdout: true).trim()
+    def patchVersion = sh(script: "mvn help:evaluate -Dexpression=parsedVersion.nextIncrementalVersion -q -DforceStdout", returnStdout: true).trim()
+
+    echo "Parsed Version: ${majorVersion}.${minorVersion}.${patchVersion}"
+
+    
+    def fullVersion = "${majorVersion}.${minorVersion}.${patchVersion}"
+    echo "Bumping PATCH version to: ${fullVersion}"
+
+   
     sh "mvn versions:set -DnewVersion=${fullVersion}"
     sh "mvn versions:commit"
+
+    
     env.IMAGE_TAG = fullVersion
-    sh 'mvn package'
+
+   
+    sh "mvn package"
 }
