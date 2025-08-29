bf6af9c521a967267f9cc95396d3a7d37815b75c
AnushSingla <singlaanush18@gmail.com>
2025-08-29 12:48:36 +0530

    small fix

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 def call(){
     echo "Building Application"
-    def version = sh(script :  "mvn help:evaluate -Dexpression=project.version -q -DforceStdout",returnStdout:true).trim()
-    def buildnumber = env.BUILD_NUMBER
-    def fullVersion = "${version}-${buildnumber}"
     sh "mvn build-helper:parse-version"
+    def majorVersion = sh(script :  "mvn help:evaluate -Dexpression=parsedVersion.majorVersion  -q -DforceStdout",returnStdout:true).trim()
+    def minorVersion = sh(script :  "mvn help:evaluate -Dexpression=parsedVersion.minorVersion  -q -DforceStdout",returnStdout:true).trim()
+    def patch = sh(script: "mvn help:evaluate -Dexpression=parsedVersion.nextIncrementalVersion -q -DforceStdout", returnStdout: true).trim()
+    def fullVersion = "${majorVersion}.${minorVersion}.${patch}"
+    
     sh "mvn versions:set -DnewVersion=${fullVersion}"
     sh "mvn versions:commit"
     env.IMAGE_TAG = fullVersion
