b9e926564ddd18c2389cdd9997526c2dedd675ea
AnushSingla <singlaanush18@gmail.com>
2025-11-16 19:50:31 +0530

    small fix

--- vars/buildVersion.groovy
+++ vars/buildVersion.groovy
@@ -11,20 +11,30 @@ def call(String folderPath = '') {
 
     patch = patch + 1
     def newversion = "${major}.${minor}.${patch}"
-
     echo "New version: ${newversion}"
 
     pkgJSON.version = newversion
     writeJSON file: packageFile, json: pkgJSON, pretty: 4
 
-    sh """
-        cd ${folderPath}
-        git config user.email "jenkins@asha-saathi.com"
-        git config user.name "Jenkins Bot Asha-Saathi"
-        git add package.json
-        git commit -m "CI: Version bump to ${newversion}"
-        git push origin HEAD:main
-    """
+    // 🔥 USE JENKINS CREDENTIALS FOR GIT PUSH
+    withCredentials([usernamePassword(
+        credentialsId: 'github-jenkins',
+        usernameVariable: 'GIT_USER',
+        passwordVariable: 'GIT_PASS'
+    )]) {
+        sh """
+            cd ${folderPath}
+            git config user.email "jenkins@asha-saathi.com"
+            git config user.name "Jenkins Bot Asha-Saathi"
+
+            git remote set-url origin https://${GIT_USER}:${GIT_PASS}@github.com/AnushSingla/Asha.git
+            
+            git add package.json
+            git commit -m "CI: Version bump to ${newversion}" || true
+
+            git push origin HEAD:main
+        """
+    }
 
     return newversion
 }
